e9f3ec077f48d8deecd86a8793b858086cf0e0ba
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-07 22:04:01 -0500

    Css styling for background

--- app.js
+++ app.js
@@ -17,12 +17,15 @@
  */
 
 
+  fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+  .then(response => response.json())
+   .then(data => {
+     console.log(data);
+     document.body.style.backgroundImage = `url(${data.urls.regular})`
 
- fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
- .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    document.body.style.backgroundImage = `url(${data.urls.regular})`
-  })
+   })
+
+
+
 
 
