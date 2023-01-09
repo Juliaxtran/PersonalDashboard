d9abba60f8545c3479e240b48adb816140ed1498
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-09 17:41:21 -0500

    Check for error response

--- app.js
+++ app.js
@@ -12,22 +12,28 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
   .catch(err => {
     // If there is an error, use a backup image
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1484542603127-984f4f7d14cb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzMyMzA0ODQ&ixlib=rb-4.0.3&q=80&w=1080)`
-    author.textContent = `Photo by  Brady Bellini`})
+    author.textContent = `Photo by  Brady Bellini`
+  })
 
 
 
-//  Fetch crypto info 
+//  Fetch crypto info
 
 fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
-.then(response => response.json())
-.then(data => {
-  console.log(data)
-  crypto[0].innerHTML =
-  `<h3>${data.name}</h3>
-  <img src="${data.image.thumb}" alt="${data.name}">
-  <p>Current Price: $${data.market_data.current_price.usd}</p>`
-})
-.catch(err => {
-  console.log(err)
-  crypto[0].innerHTML = `<h3>Unable to fetch crypto info...</h3>`
-})
+  .then(res => {
+    if (!res.ok) {
+      throw Error ("Something went wrong")
+    }
+    return res.json()
+  })
+  .then(data => {
+    console.log(data)
+    crypto[0].innerHTML =
+      `<h3>${data.name}</h3>
+       <img src="${data.image.thumb}" alt="${data.name}">
+       <p>Current Price: $${data.market_data.current_price.usd}</p>`
+  })
+  .catch(err => {
+    console.log(err)
+    crypto[0].innerHTML = `<h3>Unable to fetch crypto info...</h3>`
+  })
